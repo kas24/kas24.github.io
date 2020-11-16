72fe6562ca97c1e26406e4d495a3e619096e829c
kas24 <kas24@users.noreply.github.com>
2020-11-16 11:21:21 -0800

    Fixed Nav!!!

---
+++ Projects/P2/script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var x = document.getElementById("nav");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+function burgerTime() {
+  var myNav = document.getElementById("nav")
+   myNav.classList.toggle("hide-desktop");
+}
